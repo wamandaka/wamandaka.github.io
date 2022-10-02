7f2e412f6abb358cb88f3050179fded3bc289307
wamandaka <108261088+wamandaka@users.noreply.github.com>
2022-10-02 20:53:32 +0700

    portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,25 @@
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const myBtn = document.getElementById("myBtn")
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        myBtn.style.display = "block";
+    } else {
+        header.classList.remove('navbar-fixed');
+        myBtn.style.display = "none";
+    }
+}
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// Button Scroll To The Top
